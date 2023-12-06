f9a8b89c97298e917b42dd740468cde75cb0f84e
Minwoo Kwak <minwoo@Minwoos-MacBook-Pro.local>
2023-12-06 14:18:10 +0900

    day1 python solution

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,16 @@
-testing
+sum = 0
+
+with open("input.txt") as input:
+    for line in input:
+        firstDigit = ""
+        lastDigit = ""
+        for char in line:
+            if char.isdigit():
+                if firstDigit == "":
+                    firstDigit = char
+                else:
+                    lastDigit = char
+        digit = int(firstDigit + firstDigit) if lastDigit == "" else int(firstDigit + lastDigit)
+        sum += digit
+
+print(sum)
